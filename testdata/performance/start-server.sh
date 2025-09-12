d05c3ccc315c767234fc16c093228763337e2a53
Alliballibaba <alliballibaba@gmail.com>
2025-09-12 23:20:18 +0200

    fixes go.sh

--- testdata/performance/start-server.sh
+++ testdata/performance/start-server.sh
@@ -2,6 +2,6 @@
 
 # build and run FrankenPHP with the k6.Caddyfile
 cd /go/src/app/caddy/frankenphp &&
-	go build --buildvcs=false &&
+	../../go.sh build --buildvcs=false &&
 	cd ../../testdata/performance &&
 	/go/src/app/caddy/frankenphp/frankenphp run -c k6.Caddyfile
