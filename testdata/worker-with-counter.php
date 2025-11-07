e2b066cc2278b75e63b0b161b70d5f02b0265610
Alliballibaba <alliballibaba@gmail.com>
2025-11-07 22:50:29 +0100

    Removes the test sleep.

--- testdata/worker-with-counter.php
+++ testdata/worker-with-counter.php
@@ -4,7 +4,6 @@ $numberOfRequests = 0;
 $printNumberOfRequests = function () use (&$numberOfRequests) {
     $numberOfRequests++;
     echo "requests:$numberOfRequests";
-    sleep(1);
 };
 
 while (frankenphp_handle_request($printNumberOfRequests)) {
