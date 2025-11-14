3db2dabe220efd261e38d146f99c72c009902f6b
Kévin Dunglas <kevin@dunglas.fr>
2025-11-14 15:25:49 +0100

    fix watcher-skip

--- internal/watcher/watcher-skip.go
+++ internal/watcher/watcher-skip.go
@@ -4,7 +4,7 @@ package watcher
 
 import "log/slog"
 
-func InitWatcher(filePatterns []string, callback func(), logger *slog.Logger) error {
+func InitWatcher(ct context.Context, filePatterns []string, callback func(), logger *slog.Logger) error {
 	logger.Error("watcher support is not enabled")
 
 	return nil
