c4aedb8b85c305e88d815e748800b1f5c2614ada
Alliballibaba <alliballibaba@gmail.com>
2025-12-03 00:05:39 +0100

    Solves race condition.

--- testdata/request-sender.php
+++ testdata/request-sender.php
@@ -5,4 +5,8 @@ $workerName = $_GET["workerName"] ?? '';
 
 frankenphp_send_request($message, $workerName);
 
+// sleep to make sure request was received
+// TODO: solve this test-restart race condition with Futures instead?
+usleep(10_000);
+
 echo "request sent";
