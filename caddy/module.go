137f74cfbc50bee72a7930ab22e8de336802db29
henderkes <m@pyc.ac>
2025-12-31 12:38:55 +0100

    @dunglas suggestion

--- caddy/module.go
+++ caddy/module.go
@@ -181,9 +181,9 @@ func (f *FrankenPHPModule) ServeHTTP(w http.ResponseWriter, r *http.Request, _ c
 		if documentRoot == "" && frankenphp.EmbeddedAppPath != "" {
 			documentRoot = frankenphp.EmbeddedAppPath
 		}
-		//If we do not have a resolved document root, then we cannot resolve the symlink of our cwd because it may
-		//resolve to a different directory than the one we are currently in.
-		//This is especially important if there are workers running.
+		// If we do not have a resolved document root, then we cannot resolve the symlink of our cwd because it may
+		// resolve to a different directory than the one we are currently in.
+		// This is especially important if there are workers running.
 		documentRootOption = frankenphp.WithRequestDocumentRoot(documentRoot, false)
 	} else {
 		documentRoot = f.resolvedDocumentRoot
