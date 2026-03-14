49fc8784f5276d8b703447b25a111414bb356bd7
henderkes <m@pyc.ac>
2026-03-14 10:37:00 +0700

    override opcache reset handler for every php thread in php 8.2

--- frankenphp.c
+++ frankenphp.c
@@ -1232,6 +1232,7 @@ bool frankenphp_new_php_thread(uintptr_t thread_index) {
 
 static int frankenphp_request_startup() {
   frankenphp_update_request_context();
+  frankenphp_override_opcache_reset();
   if (php_request_startup() == SUCCESS) {
     return SUCCESS;
   }
