ed3170ddd89a355f097c8efc7b3dec318cf77552
Indra Gunawan <hello@indra.my.id>
2026-03-17 18:01:49 +0800

    revert extinit.go

--- caddy/extinit.go
+++ caddy/extinit.go
@@ -3,42 +3,46 @@ package caddy
 import (
 	"errors"
 	"log"
+	"os"
 	"path/filepath"
 	"strings"
 
-	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 	"github.com/dunglas/frankenphp/internal/extgen"
+
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	caddycmd.RegisterCommand(caddycmd.Command{
 		Name:  "extension-init",
-		Usage: "go_extension.go",
+		Usage: "go_extension.go [--verbose]",
 		Short: "Initializes a PHP extension from a Go file (EXPERIMENTAL)",
 		Long: `
 Initializes a PHP extension from a Go file. This command generates the necessary C files for the extension, including the header and source files, as well as the arginfo file.`,
 		CobraFunc: func(cmd *cobra.Command) {
-			cmd.RunE = cmdInitExtension
+			cmd.Flags().BoolP("debug", "v", false, "Enable verbose debug logs")
+
+			cmd.RunE = caddycmd.WrapCommandFuncForCobra(cmdInitExtension)
 		},
 	})
 }
 
-func cmdInitExtension(cmd *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		return errors.New("the path to the Go source is required")
+func cmdInitExtension(_ caddycmd.Flags) (int, error) {
+	if len(os.Args) < 3 {
+		return 1, errors.New("the path to the Go source is required")
 	}
 
-	sourceFile := args[0]
+	sourceFile := os.Args[2]
 	baseName := extgen.SanitizePackageName(strings.TrimSuffix(filepath.Base(sourceFile), ".go"))
 
 	generator := extgen.Generator{BaseName: baseName, SourceFile: sourceFile, BuildDir: filepath.Dir(sourceFile)}
 
 	if err := generator.Generate(); err != nil {
-		return err
+		return 1, err
 	}
 
 	log.Printf("PHP extension %q initialized successfully in directory %q", baseName, generator.BuildDir)
 
-	return nil
+	return 0, nil
 }
