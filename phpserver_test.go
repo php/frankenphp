adf2feda440bec70365e5ccb46f634cdd86aa6b2
Alliballibaba <alliballibaba@gmail.com>
2026-07-02 13:15:52 +0200

    refines tests.

--- phpserver_test.go
+++ phpserver_test.go
@@ -32,10 +32,6 @@ func initPhpServerWithOptions(t *testing.T, idx int, serverOpts ...frankenphp.Ph
 	return frankenphp.PhpServers[idx]
 }
 
-func phpServerProbeURL(pathInfo string) string {
-	return "http://example.com/server-variable.php" + pathInfo
-}
-
 func phpServerRequest(t *testing.T, server *frankenphp.PhpServer, req *http.Request) (string, *http.Response) {
 	t.Helper()
 
@@ -74,8 +70,8 @@ func TestPhpServer(t *testing.T) {
 			),
 		)
 
-		body1, _ := phpServerGet(t, frankenphp.PhpServers[1], phpServerProbeURL(""))
-		body2, _ := phpServerGet(t, frankenphp.PhpServers[2], phpServerProbeURL(""))
+		body1, _ := phpServerGet(t, frankenphp.PhpServers[1], "http://example.com/server-variable.php")
+		body2, _ := phpServerGet(t, frankenphp.PhpServers[2], "http://example.com/server-variable.php")
 
 		assert.Contains(t, body1, "[PHP_SERVER_IDX] => 1")
 		assert.Contains(t, body2, "[PHP_SERVER_IDX] => 2")
@@ -97,19 +93,19 @@ func TestPhpServer(t *testing.T) {
 			"PHP_SERVER_TEST_KEY": "from_php_server",
 		}))
 
-		body, _ := phpServerGet(t, server, phpServerProbeURL(""))
+		body, _ := phpServerGet(t, server, "http://example.com/server-variable.php")
 
 		assert.Contains(t, body, "[PHP_SERVER_TEST_KEY] => from_php_server")
 	})
 
 	t.Run("split_path", func(t *testing.T) {
-		server := initPhpServerWithOptions(t, 1, frankenphp.WithPhpServerSplitPath([]string{".php"}))
+		server := initPhpServerWithOptions(t, 1, frankenphp.WithPhpServerSplitPath([]string{".custom"}))
 
-		body, _ := phpServerGet(t, server, "http://example.com/server-globals.php/pathinfo")
+		body, _ := phpServerGet(t, server, "http://example.com/split-path.custom/pathinfo")
 
 		assert.Contains(t, body, "PATH_INFO: /pathinfo\n")
-		assert.Contains(t, body, "SCRIPT_NAME: /server-globals.php\n")
-		assert.Contains(t, body, "PHP_SELF: /server-globals.php/pathinfo\n")
+		assert.Contains(t, body, "SCRIPT_NAME: /split-path.custom\n")
+		assert.Contains(t, body, "PHP_SELF: /split-path.custom/pathinfo\n")
 	})
 
 	t.Run("logger", func(t *testing.T) {
@@ -126,34 +122,34 @@ func TestPhpServer(t *testing.T) {
 		assert.Contains(t, buf.String(), "request handling started", "should contain the debug message from worker start")
 	})
 
-	t.Run("workers_by_path", func(t *testing.T) {
-		initPhpServer(t, frankenphp.WithPhpServer(1,
-			frankenphp.WithPhpServerRoot(testDataDir, false),
-			frankenphp.WithPhpServerWorker("counter", testDataDir+"worker-with-counter.php", 1),
-		))
+	t.Run("workers_by_path_and_request_matcher", func(t *testing.T) {
+		initPhpServer(
+			t,
+			frankenphp.WithPhpServer(1,
+				frankenphp.WithPhpServerRoot(testDataDir, false),
+				frankenphp.WithPhpServerWorker("counter", testDataDir+"worker-with-counter.php", 1),
+			),
+			frankenphp.WithPhpServer(2,
+				frankenphp.WithPhpServerRoot(testDataDir, false),
+				frankenphp.WithPhpServerWorker("match", testDataDir+"worker-with-counter.php", 1,
+					frankenphp.WithWorkerMatchOn(func(r *http.Request) bool {
+						return strings.HasPrefix(r.URL.Path, "/match/")
+					}),
+				),
+			),
+		)
 
 		body1, _ := phpServerGet(t, frankenphp.PhpServers[1], "http://example.com/worker-with-counter.php")
 		body2, _ := phpServerGet(t, frankenphp.PhpServers[1], "http://example.com/worker-with-counter.php")
+		body3, _ := phpServerGet(t, frankenphp.PhpServers[2], "http://example.com/match/anything")
+		body4, _ := phpServerGet(t, frankenphp.PhpServers[2], "http://example.com/match/anything")
+		body5, _ := phpServerGet(t, frankenphp.PhpServers[2], "http://example.com/index.php")
 
-		assert.Equal(t, "requests:1", body1)
-		assert.Equal(t, "requests:2", body2)
-	})
-
-	t.Run("workers_with_request_matcher", func(t *testing.T) {
-		initPhpServer(t, frankenphp.WithPhpServer(1,
-			frankenphp.WithPhpServerRoot(testDataDir, false),
-			frankenphp.WithPhpServerWorker("match", testDataDir+"dedup-match-worker.php", 1,
-				frankenphp.WithWorkerMatchOn(func(r *http.Request) bool {
-					return strings.HasPrefix(r.URL.Path, "/match/")
-				}),
-			),
-		))
-
-		matched, _ := phpServerGet(t, frankenphp.PhpServers[1], "http://example.com/match/anything")
-		regular, _ := phpServerGet(t, frankenphp.PhpServers[1], "http://example.com/server-globals.php")
-
-		assert.Equal(t, "dedup-match-worker", matched)
-		assert.Contains(t, regular, "SCRIPT_NAME: /server-globals.php\n")
+		assert.Equal(t, "requests:1", body1, "should contain the counter for the first worker")
+		assert.Equal(t, "requests:2", body2, "should contain the counter for the first worker")
+		assert.Equal(t, "requests:1", body3, "should contain the counter for the second worker")
+		assert.Equal(t, "requests:2", body4, "should contain the counter for the second worker")
+		assert.Contains(t, body5, "I am by birth a Genevese (i not set)", "should fall back to (non-worker) index.php")
 	})
 
 	t.Run("worker_inherits_env", func(t *testing.T) {
@@ -180,7 +176,7 @@ func TestPhpServer(t *testing.T) {
 			),
 		)
 
-		body, _ := phpServerGet(t, frankenphp.PhpServers[1], phpServerProbeURL(""))
+		body, _ := phpServerGet(t, frankenphp.PhpServers[1], "http://example.com/server-variable.php")
 
 		assert.Contains(t, body, "[PHP_SERVER_IDX] => first")
 		assert.NotContains(t, body, "[PHP_SERVER_IDX] => second")
@@ -189,7 +185,7 @@ func TestPhpServer(t *testing.T) {
 	t.Run("serve_http_validation", func(t *testing.T) {
 		server := initPhpServerWithOptions(t, 1)
 
-		req := httptest.NewRequest(http.MethodGet, phpServerProbeURL(""), nil)
+		req := httptest.NewRequest(http.MethodGet, "http://example.com/server-variable.php", nil)
 		req.Header.Add("Content-Length", "-1")
 		body, resp := phpServerRequest(t, server, req)
 
@@ -199,7 +195,7 @@ func TestPhpServer(t *testing.T) {
 
 	t.Run("not_running", func(t *testing.T) {
 		server := &frankenphp.PhpServer{}
-		req := httptest.NewRequest(http.MethodGet, phpServerProbeURL(""), nil)
+		req := httptest.NewRequest(http.MethodGet, "http://example.com/server-variable.php", nil)
 		w := httptest.NewRecorder()
 
 		err := server.ServeHTTP(w, req)
