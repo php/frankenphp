0ddd12c5e86e8864bced98da0c44d4a1dfc49db9
Ousama Ben Younes <benyounes.ousama@gmail.com>
2026-07-24 09:39:49 +0000

    fix: simplify PHP_CONFIG handling in go.sh

--- go.sh
+++ go.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 # Runs the go command with the proper Go and cgo flags.
 
-readonly DEFAULT_PHP_CONFIG="php-config"
-PHP_CONFIG="${PHP_CONFIG:-${DEFAULT_PHP_CONFIG}}"
-
 GOFLAGS="$GOFLAGS -tags=nobadger,nomysql,nopgx" \
-	CGO_CFLAGS="$CGO_CFLAGS $("${PHP_CONFIG}" --includes) $(sh "$(dirname "$0")/mtls-cflags.sh")" \
-	CGO_LDFLAGS="$CGO_LDFLAGS $("${PHP_CONFIG}" --ldflags) $("${PHP_CONFIG}" --libs)" \
+	CGO_CFLAGS="$CGO_CFLAGS $(${PHP_CONFIG:-php-config} --includes) $(sh "$(dirname "$0")/mtls-cflags.sh")" \
+	CGO_LDFLAGS="$CGO_LDFLAGS $(${PHP_CONFIG:-php-config} --ldflags) $(${PHP_CONFIG:-php-config} --libs)" \
 	go "$@"
