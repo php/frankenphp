98aadc42ffa686178e14347ff804f71a92bc0abf
henderkes <m@pyc.ac>
2026-07-29 17:43:21 +0700

    clang-format

--- frankenphp.h
+++ frankenphp.h
@@ -74,7 +74,8 @@ typedef struct {
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
-/* phpinfo entries from Go - null-terminated array of key, value, key, value, ... */
+/* phpinfo entries from Go - null-terminated array of key, value, key, value,
+ * ... */
 extern const char **frankenphp_phpinfo_entries;
 
 typedef struct go_string {
