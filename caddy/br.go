ce102e52907801ce3bcdde0f5daa4995dcb5939c
henderkes <m@pyc.ac>
2026-07-29 16:53:58 +0700

    report e-dant/watcher, dunglas/caddy-cbrotli, libbrotli and dunglas/mercure versions

--- caddy/br.go
+++ caddy/br.go
@@ -2,4 +2,33 @@
 
 package caddy
 
+// #include <brotli/encode.h>
+import "C"
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/dunglas/frankenphp"
+)
+
 var brotli = true
+
+func init() {
+	brotliVer := C.BrotliEncoderVersion()
+	if brotliVer != 0 {
+		major := int(brotliVer >> 24)
+		minor := int((brotliVer >> 12) & 0xfff)
+		patch := int(brotliVer & 0xfff)
+		frankenphp.AddPhpinfoEntry("libbrotli", fmt.Sprintf("%d.%d.%d", major, minor, patch))
+	}
+
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		for _, dep := range buildInfo.Deps {
+			if dep.Path == "github.com/dunglas/caddy-cbrotli" {
+				frankenphp.AddPhpinfoEntry("dunglas/caddy-cbrotli", dep.Version)
+				break
+			}
+		}
+	}
+}
