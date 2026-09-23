1a337b019a1d0c82b848c1a1e19a6d63cc0f1e50
henderkes <m@pyc.ac>
2026-09-23 08:30:54 +0200

    keep extension array reachable while C reads it

--- internal/testext/exttest.go
+++ internal/testext/exttest.go
@@ -14,6 +14,7 @@ import "C"
 import (
 	"io"
 	"net/http/httptest"
+	"runtime"
 	"testing"
 	"unsafe"
 
@@ -25,6 +26,27 @@ import (
 func testRegisterExtension(t *testing.T) {
 	frankenphp.RegisterExtension(unsafe.Pointer(&C.module1_entry))
 	frankenphp.RegisterExtension(unsafe.Pointer(&C.module2_entry))
+
+	// race the GC against C reading the raw array
+	stop := make(chan struct{})
+	go func() {
+		var sink [][]unsafe.Pointer
+		for {
+			select {
+			case <-stop:
+				return
+			default:
+			}
+			runtime.GC()
+			for i := 0; i < 20000; i++ {
+				sink = append(sink, make([]unsafe.Pointer, 2))
+				if len(sink) > 100000 {
+					sink = nil
+				}
+			}
+		}
+	}()
+	defer close(stop)
 
 	err := frankenphp.Init()
 	require.Nil(t, err)
